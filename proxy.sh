f3a3a81e73e4a99f0956365be450f0d837565669
catitw <catitw@outlook.com>
2025-08-26 17:09:57 +0800

    update(proxy): update proxy script

--- proxy.sh
+++ proxy.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-set -ex
+set -exuo pipefail
 
 # https://nixos-and-flakes.thiscute.world/zh/nix-store/add-binary-cache-servers#accelerate-package-downloads-via-a-proxy-server
 
-sudo mkdir -p /run/systemd/system/nix-daemon.service.d/
-sudo cat <<EOF >/run/systemd/system/nix-daemon.service.d/override.conf
+if [ "$EUID" -ne 0 ]; then
+  exec sudo -p "[sudo] enter password to continue: " -- "$0" "$@"
+fi
+
+mkdir -p /run/systemd/system/nix-daemon.service.d/
+cat <<EOF >/run/systemd/system/nix-daemon.service.d/override.conf
 [Service]
 Environment="https_proxy=http://10.0.1.124:7897"
 EOF
-sudo systemctl daemon-reload
-sudo systemctl restart nix-daemon
+systemctl daemon-reload
+systemctl restart nix-daemon
