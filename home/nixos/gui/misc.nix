068904f4b33fc27f16b12c9a32f6cee8abc7ec70
Xy Gu <catitw@outlook.com>
2025-09-03 16:22:20 +0800

    feat(vscode): add `vscode` program

--- home/nixos/gui/misc.nix
+++ home/nixos/gui/misc.nix
@@ -6,4 +6,11 @@
   home.packages = with pkgs; [
     feishu
   ];
+
+  # [Use VS Code extensions without additional configuration](https://nixos.wiki/wiki/Visual_Studio_Code#Use_VS_Code_extensions_without_additional_configuration)
+  programs.vscode = {
+    enable = true;
+    package = pkgs.vscode.fhs;
+  };
+  catppuccin.vscode.enable = false;
 }
