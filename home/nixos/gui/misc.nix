487a88a77ffa96ea3561bbda854e77eb65986366
Xy Gu <catitw@outlook.com>
2025-09-04 14:17:24 +0800

    fix(vscode): disable fhs

--- home/nixos/gui/misc.nix
+++ home/nixos/gui/misc.nix
@@ -10,7 +10,6 @@
   # [Use VS Code extensions without additional configuration](https://nixos.wiki/wiki/Visual_Studio_Code#Use_VS_Code_extensions_without_additional_configuration)
   programs.vscode = {
     enable = true;
-    package = pkgs.vscode.fhs;
   };
-  catppuccin.vscode.enable = false;
+  catppuccin.vscode.profiles.default.enable = false;
 }
