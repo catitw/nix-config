30ca4259b72734dc99a34d415e24b87e06156665
Xy Gu <catitw@outlook.com>
2025-09-12 14:24:15 +0800

    update(opencode): add mcp-context7 for opencode

--- home/nixos/tui/opencode.nix
+++ home/nixos/tui/opencode.nix
@@ -9,16 +9,24 @@
     config.lib.file.mkOutOfStoreSymlink
       osConfig.sops.templates."opencode-auth.json".path;
 
-  programs.opencode = {
-    enable = true;
-    # [options](https://mynixos.com/home-manager/option/programs.opencode.settings)
-    settings = {
-      provider = {
-        zhipuai = {
-          # https://docs.z.ai/scenario-example/develop-tools/opencode#using-opencode-with-the-glm-coding-plan
-          api = "https://open.bigmodel.cn/api/paas/v4";
-        };
-      };
-    };
-  };
+  home.file.".config/opencode/config.jsonc".source =
+    config.lib.file.mkOutOfStoreSymlink
+      osConfig.sops.templates."opencode-config.jsonc".path;
+
+  home.packages = with pkgs; [
+    opencode
+  ];
+
+  # programs.opencode = {
+  #   enable = true;
+  #   # [options](https://mynixos.com/home-manager/option/programs.opencode.settings)
+  #   settings = {
+  #     provider = {
+  #       zhipuai = {
+  #         # https://docs.z.ai/scenario-example/develop-tools/opencode#using-opencode-with-the-glm-coding-plan
+  #         api = "https://open.bigmodel.cn/api/paas/v4";
+  #       };
+  #     };
+  #   };
+  # };
 }
--- secrets/nixos.nix
+++ secrets/nixos.nix
@@ -34,6 +34,15 @@
         group = "root";
         mode = "0400";
       };
+      "mcp_context7_key" = {
+        sopsFile = "${my-secrets}/mcp.yaml";
+        format = "yaml";
+        # https://discourse.nixos.org/t/sops-nix-secrets-yaml-is-not-valid-and-key-cannot-be-found/68071/5
+        key = "context7/api_key";
+        owner = "root";
+        group = "root";
+        mode = "0400";
+      };
     };
 
     templates = {
@@ -44,6 +53,32 @@
               "type": "api",
               "key": "${config.sops.placeholder.zhipu_ai_key}"
             }
+          }
+        '';
+        owner = myvars.username;
+        group = myvars.username;
+        mode = "0600";
+      };
+      "opencode-config.jsonc" = {
+        content = ''
+          {
+            "$schema": "https://opencode.ai/config.json",
+            "provider": {
+              "zhipuai": {
+                "api": "https://open.bigmodel.cn/api/paas/v4"
+              }
+            },
+            "mcp": {
+                // https://github.com/upstash/context7?tab=readme-ov-file#opencode-remote-server-connection
+                "context7": {
+                  "type": "remote",
+                  "url": "https://mcp.context7.com/mcp",
+                  "headers": {
+                    "CONTEXT7_API_KEY": "${config.sops.placeholder.mcp_context7_key}"
+                  },
+                  "enabled": true
+                }
+            },
           }
         '';
         owner = myvars.username;
