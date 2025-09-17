f519b70d433621d3b7cee885329d712a123f7aa2
Xy Gu <catitw@outlook.com>
2025-09-17 11:09:47 +0800

    update(vscode): add `codelldb` plugin

--- home/nixos/gui/misc.nix
+++ home/nixos/gui/misc.nix
@@ -10,6 +10,9 @@
   # [Use VS Code extensions without additional configuration](https://nixos.wiki/wiki/Visual_Studio_Code#Use_VS_Code_extensions_without_additional_configuration)
   programs.vscode = {
     enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      vadimcn.vscode-lldb
+    ];
   };
   catppuccin.vscode.profiles.default.enable = false;
 }
